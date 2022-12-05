c8b4a2edee50f9d024aebee08f16047fb2aa04fb
Bud Davis <jimmie.davis@uta.edu>
2022-12-05 19:17:39 +0000

    work in progress

--- examples/mmap.c
+++ examples/mmap.c
@@ -5,8 +5,8 @@
     To make the sample file:
 
      % # create the file system image
-     % dd if=/dev/zero of=test.data count=1 bs=1G
-     % sudo losetup /dev/loop2 test.data
+     % dd if=/dev/zero of=test.image count=1 bs=1G
+     % sudo losetup /dev/loop2 test.image
      % sudo /usr/sbin/mkexfatfs /dev/loop2
 
      % # put something in the file system image
@@ -18,6 +18,7 @@
      % sudo umount /tmp/d
      % sudo losetup -d /dev/loop2
      % rm -rf /tmp/d
+     % rm test.image
 
 
 
@@ -27,17 +28,40 @@
     other than this course is prohibited.
 
 */
-
+ 
 #include <stdio.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
 
 int main(int argc, char *argv[])
 {
+
     // open the file system image file
-    int fd = 
+    int fd = open("test.image",O_RDWR);
+    printf("file descriptor = %d\n",fd);
+    assert(fd);
+    
     // mmap it
+    void *p = mmap(NULL,
+                      0,
+                   PROT_NONE,
+                   0,
+                   fd,
+                   0);
+    assert(p);
+
     // print out some things we care about
 
+
     printf("hello\n");
+    int closeStat = close(fd);
+    if (closeStat)
+       perror("closeStat");
+    fd = 0;
     return 0;
 }
